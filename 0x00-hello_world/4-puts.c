785a77e6ea6930d2a46b95bc41f0f7ae223b8c14
watch14 <watchman1414@gmail.com>
2023-07-13 15:00:27 +0000

    fast_push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (void){
 	/*coment
-	 * commt2 */
-	puts("\"Programming is like building a multilingual puzzle");
+	 * comimt2 */
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
