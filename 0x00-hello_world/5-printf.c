1bbb43a210260158e739c73c03af7d565550be98
watch14 <watchman1414@gmail.com>
2023-07-13 15:17:38 +0000

    fast_push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * main - display with proper grammar, but the outcome is a piece of art,
- *
- * Return: 0 success.
+ * main -display with proper grammar, but the outcome is a piece of art,
+ * Return 0 
  */
 int main(void)
 {
-
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
