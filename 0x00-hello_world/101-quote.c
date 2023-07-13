368dd3a24876d0b78e62e6992a013a3e0e439901
watch14 <watchman1414@gmail.com>
2023-07-13 16:13:53 +0000

    fast_push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,7 @@
 int main(void)
 {
 
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	printf("\n");
+	write(and that piece of art is useful\" - Dora Korpar, 2015-10-19);
 
 	return (1);
 
