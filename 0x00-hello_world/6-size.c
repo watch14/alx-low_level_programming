e08c9ca0230eb0074a199557c035e8f407b0202b
watch14 <watchman1414@gmail.com>
2023-07-13 15:32:30 +0000

    fast_push

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/***/
+
+ int main(void){
+
+	 printf("Size of a char %d\n",sizeof(char));
+	 printf("Size of a int %d\n",sizeof(int));
+	 printf("Size of a long int %d\n",sizeof(long int));
+	 printf("Size of a long long int %d\n",sizeof(long long int));
+	 printf("Size of a float int %d\n",sizeof(float));
+
+	 return(0);
+ }
