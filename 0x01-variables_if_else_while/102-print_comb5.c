d3d38006fc7ca68beb323bfd57759608258e4ad7
watch14 <watchman1414@gmail.com>
2023-07-14 15:30:39 +0000

    fast_push

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,22 +12,17 @@ int main(void)
 	{
 		for (b = 0; b < 100; b++)
 		{
-			if (a < b)
-			{
-				putchar((a / 10) + '0');
-				putchar((a % 10) + '0');
-				putchar(' ');
-				putchar((b / 10) + '0');
-				putchar((b % 10) + '0');
-				if (a == 98 && b == 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
+				break;
+			putchar(',');
+			putchar(' ');
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
