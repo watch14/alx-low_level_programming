582855db18e87b24aed577e546d0aed8d6d3e63d
watch14 <watchman1414@gmail.com>
2023-07-14 11:02:22 +0000

    fast_push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	/* your code goes there */
 	if(n > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5 ", n, n % 10);
+	printf("Last digit of %d is %d and is greater than 5", n, n % 10);
 	} else if(n == 0)
 	{
 		printf("Last digit of %d is %d and is 0 ", n, n % 10);
