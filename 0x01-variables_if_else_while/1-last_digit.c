cd2235aa85c1e90cd6a3b6a5fb5b4ef98f8a5b22
watch14 <watchman1414@gmail.com>
2023-07-14 10:59:07 +0000

    fast_push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,13 +17,13 @@ int main(void)
 	/* your code goes there */
 	if(n > 5)
 	{
-	printf("%d is greater than 5", n % 10);
+	printf("Last digit of %d is %d and is greater than 5", n, n % 10);
 	} else if(n == 0)
 	{
-		printf("%d is greater than 5", n % 10);
+		printf("Last digit of %d is %d and is greater than 5", n, n % 10);
 	} else
 	{
-		printf("%d  is less than 6 and not 0",n % 10);
+		printf("Last digit of %d is %d is less than 6 and not 0", n, n % 10);
 	}
 	return (0);
 }
