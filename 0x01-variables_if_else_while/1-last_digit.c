de42beda477d22b916e9bac51c4eb164b5bad5e6
watch14 <watchman1414@gmail.com>
2023-07-14 11:12:42 +0000

    fast_push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,17 +16,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	printf("Lats digit of %d ",n);
+	printf("Lats digit of %d ", n);
 	if (n > 5)
-	{
 	printf("is %d and is greater than 5", n % 10);
-	}
 	else if (n == 0)
-	{
 		printf("is %d and is 0 ",n % 10);
-	}
-	else{
+	else
 		printf("is %d and is less than 6 and not 0 ", n % 10);
-	}
 	return (0);
 }
