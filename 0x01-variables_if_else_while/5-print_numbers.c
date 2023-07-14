5d4c1a857cf03da2e9bf9af620ca931224131963
watch14 <watchman1414@gmail.com>
2023-07-14 12:48:19 +0000

    fast_push

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,9 +6,8 @@
  */
 int main(void)
 {
-    int i = 0;
-
-        for (; i<10 ; i++)
-        printf("%d\n",i);
-        return (0);
+	int i = 0;
+	for (; i<10 ; i++)
+		printf("%d\n",i);
+	return (0);
 }
