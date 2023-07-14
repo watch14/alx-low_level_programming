4a2ffdfca2c397ffcf7e9d2b19d39fb01ab740e8
watch14 <watchman1414@gmail.com>
2023-07-14 14:36:16 +0000

    fast_push

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1 ; b <= '9' ; b++)
+		{
+			for (c = b + 1 ; c <= '9' ; b++){
+			putchar (a);
+			putchar (b);
+			if (a == '8' && b == '9')
+				break;
+			putchar (',');
+			putchar (' ');
+		}
+		}
+
+	}
+	putchar ('\n');
+	return (0);
+}
