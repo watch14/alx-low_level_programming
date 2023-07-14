da7c7e948979cae3d5874f295f3dcabe72fb35ad
watch14 <watchman1414@gmail.com>
2023-07-14 12:58:39 +0000

    fast_push

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+int main(void)
+{
+	int i = 0
+
+	for (; i = 9 ; i++)
+	{
+		putchar (i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
