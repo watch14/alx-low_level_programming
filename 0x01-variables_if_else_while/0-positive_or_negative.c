fdd52a435e96927909d5ed5a4d7b7348a1da6d28
watch14 <watchman1414@gmail.com>
2023-07-14 09:44:49 +0000

    fast_push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,13 +20,12 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	}else if (n == 0)
+	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	} else
-	{ 
+	{
 		printf("%d is negative\n", n);
 	}
-	
 	return (0);
 }
