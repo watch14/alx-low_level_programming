07c96bc5d2953bb00c5590db54a8c4855415cf10
watch14 <watchman1414@gmail.com>
2023-07-14 12:17:36 +0000

    fast_push

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,17 @@
  * Return: 0 indicate success.
  */
 
-int main(void){
+int main(void)
+{
 	int a = 'a';
 	int A = 'A';
 
-	for (; a <= 'z'; a++){
+	for (; a <= 'z'; a++)
+	{
 		putchar(a);
 	}
-	for (; A <= 'Z'; A++){
+	for (; A <= 'Z'; A++)
+	{
 		putchar(A);
 	}
 	putchar ('\n');
