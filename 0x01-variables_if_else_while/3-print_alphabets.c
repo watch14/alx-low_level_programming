f67761ab473835020a934375e442a3b5a2527680
watch14 <watchman1414@gmail.com>
2023-07-14 11:59:43 +0000

    fast_push

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+
+int main(void){
+	int a='a';
+	int A='A';
+
+	for(;a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	for(;A <= 'Z';A++)
+	{
+		putchar(Z);
+	}
+return (0);
+}
