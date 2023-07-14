2d56f1b89c0907db7cf65bd03196f78457e86613
watch14 <watchman1414@gmail.com>
2023-07-14 12:27:29 +0000

    fast_push

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+int main(void)
+{
+	for(int a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+		while(;a == 101 || a == 113)
+			a++;
+	}
+	putchar('\n');
+	return (0);
+}
