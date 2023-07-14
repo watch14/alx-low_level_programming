5b2ec20be99a5b06a107ae694fe0ed4aebf05639
watch14 <watchman1414@gmail.com>
2023-07-14 12:47:19 +0000

    fast_push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+int main(void)
+{
+    int i = 0;
+
+        for (; i<10 ; i++)
+        printf("%d",i);
+        return (0);
+}
