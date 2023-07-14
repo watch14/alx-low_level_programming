ab48d8e0266dffa565aa5dd0e4547e6102b3b8a7
watch14 <watchman1414@gmail.com>
2023-07-14 13:35:03 +0000

    fast_push

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,15 +4,16 @@
  *
  * Return: 0 indicate success.
  */
-int main(void)
+int main (void)
 {
 	int i = '0';
 
-	for(; i <= 102 ; i++)
+	for (; i <= 102 ; i++)
 	{
 		putchar(i);
 		if (i == 57)
 			i = i + 39;
 	}
+	putchar ('\n');
 	return (0);
 }
