93d86db0ed341afc990ff74415a9034d6f79b259
watch14 <watchman1414@gmail.com>
2023-07-14 13:05:29 +0000

    fast_push

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+int main(void)
+{
+	for (int a = 'z'; a <= 'a';a-- )
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
