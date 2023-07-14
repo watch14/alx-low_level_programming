52d79ebc582102dead6048a7f234cb8241a1dbad
watch14 <watchman1414@gmail.com>
2023-07-14 15:08:38 +0000

    fast_push

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,15 +8,17 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a < 100 ; a++)
 	{
-		for (b = a ; b <= '9' ; b++)
+		for (b = 0 ; b < 100 ; b++)
 		{
-			putchar (a);
-			putchar (b);
+			putchar ((a / 10) + 48);
+			putchar ((b / 10) + 48);
 			putchar (' ');
-			putchar (a);
-			putchar (b);
+			putchar ((a / 10) + 48); 
+			putchar ((b / 10) + 48);
+			if (a == 98 && b == 99)
+				break
 			putchar (',');
 			putchar (' ');
 		}
