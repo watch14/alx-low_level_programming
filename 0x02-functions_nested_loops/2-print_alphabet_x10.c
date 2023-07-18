2d3f18992fa9ecab0eb5125a3505b4f3e11e2421
watch14 <watchman1414@gmail.com>
2023-07-18 14:15:31 +0000

    fast_push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
 
 void print_alphabet_x10(void)
 {
-        int i , a;
-
-        for(i = 0 ; i < 10 ; i++)
-        {
-                for(a = 'a' ; a <= 'z' ; a++)
-                        _putchar(a);
-                _putchar('\n');
-        }
+	int i , a;
+	
+	for(i = 0 ; i < 10 ; i++)
+	{
+		for (a = 'a' ; a <= 'z' ; a++)
+			_putchar (a);
+		_putchar ('\n');
+	}
 }
