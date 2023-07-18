f8c2fe8fb7565bb39b8eb6b41a32c4561f6a41b6
watch14 <watchman1414@gmail.com>
2023-07-18 14:51:21 +0000

    fast_push

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stfio.h>
+/**
+ * _isalpha - Checks if a character is an alphabetic letter.
+ *
+ * Return: 1 if the character is an alphabetic letter
+ *          0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
