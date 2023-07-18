389fc907a5c66ad6b2030a96f8a3c6b60e43cdd6
watch14 <watchman1414@gmail.com>
2023-07-18 15:01:43 +0000

    fast_push

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: The number to check.
+ *
+ * Return: 1 and prints + if n is greater than zero,
+ *         0 and prints 0 if n is zero,
+ *        -1 and prints - if n is less than zero.
+ */
+
+int print_sign(int n)
+{
+	if (i > '0')
+	{
+		printf("+");
+		return (1);
+	}
+	if (i == '0')
+	{
+		printf("0");
+		return (0);
+	}
+	if (i < '0')
+        {
+                printf("-");
+                return (-1);
+        }
+}
