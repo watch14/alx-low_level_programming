26f2a89b4d12b6e34225e653ca6186aff87ca64b
watch14 <watchman1414@gmail.com>
2023-07-18 14:14:09 +0000

    fast_push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(void)
+#include <stdio.h>
+#include "main.h"
+
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+        int i , a;
+
+        for(i = 0 ; i < 10 ; i++)
+        {
+                for(a = 'a' ; a <= 'z' ; a++)
+                        _putchar(a);
+                _putchar('\n');
+        }
 }
