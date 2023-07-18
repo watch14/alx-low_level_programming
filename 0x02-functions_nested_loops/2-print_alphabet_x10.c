a479f278024ebf5753f16324d14fbb0618f08143
watch14 <watchman1414@gmail.com>
2023-07-18 14:18:36 +0000

    fast_push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
-#include <stdio.h>
-#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: void.
+ */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10 (void)
 {
 	int i, a;
 
