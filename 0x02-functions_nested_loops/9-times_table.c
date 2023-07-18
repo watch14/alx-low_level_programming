a9540515bcaf9c3c09a254acb5a7e46c076d6cb5
watch14 <watchman1414@gmail.com>
2023-07-18 19:23:15 +0000

    fast_push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,9 +4,9 @@
 void times_table(void)
 {
 	int a, b, r;
-	for(a = 0 ; a <= 9 ; a++)
+	for (a = 0 ; a <= 9 ; a++)
 	{
-		for(b = 0 ; b <= 9 ; b++ )
+		for (b = 0 ; b <= 9 ; b++ )
 		{
 			r = a * b;
 
@@ -20,19 +20,12 @@ void times_table(void)
 				_putchar ((r / 10) + '0');
 				_putchar ((r % 10) + '0');
 			}
-			if(b == 9)
+			if (b == 9)
 				break;
-			putchar(',');
-			putchar(' ');
-			putchar(' ');
+			putchar (',');
+			putchar (' ');
+			putchar (' ');
 		}
 		_putchar ('\n');
-				
-
-		if(b == 9)
-			break;
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
 	}
 }
