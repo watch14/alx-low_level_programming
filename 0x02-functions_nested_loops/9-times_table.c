ec0af852c246c5056369cafc93f3d750fbc910d6
watch14 <watchman1414@gmail.com>
2023-07-18 19:32:10 +0000

    fast_push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,6 +12,7 @@ void times_table(void)
 
 			if (r < 10)
 			{
+				_putchar (' ');
 				_putchar ((r % 10) + '0');
 				}
 			else
@@ -22,7 +23,6 @@ void times_table(void)
 			if (b == 9)
 				break;
 			_putchar (',');
-			_putchar (' ');
 			_putchar (' ');
 		}
 		_putchar ('\n');
