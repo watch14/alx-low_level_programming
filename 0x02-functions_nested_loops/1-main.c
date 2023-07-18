0faf7ecb9e0b46314c94c3e749604f83d6a2d7de
watch14 <watchman1414@gmail.com>
2023-07-18 14:00:37 +0000

    fast_push

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -7,12 +7,12 @@
  * Return: Always 0.
  */
 
-int print_alphabet(void)
+void print_alphabet(void)
 {
         int a;
 
-	for (a = 'a'; a <= 'z'; a++)
-		_putchar(a);
-	_putchar('\n');
+        for (a = 'a'; a <= 'z'; a++)
+                _putchar(a);
+        _putchar('\n');
         return (0);
 }
