9a2ab981d38b6c71e2cf018df11e15205362c887
watch14 <watchman1414@gmail.com>
2023-07-18 14:40:36 +0000

    fast_push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
-
+/**
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if the character is lowercase, 0 otherwise.
+ */
 int _islower(int c)
 {
 	if (c < 'a' || c > 'z')
