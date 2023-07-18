8d49983b783ebed3289713a3e46ecce98fd08047
watch14 <watchman1414@gmail.com>
2023-07-18 17:23:31 +0000

    fast_push

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+void jack_bauer(void)
+{
+	int h ,m;
+
+	for (h = '0' ; h <= '9' ; h++)
+		pucthar(h);
+		 for (h = '0' ; h <= '9' ; h++)
+		 {
+			 {
+				 pucthar(h);
+				 for (m = '0' ; m <= '9' ; m++)
+				 {
+					 putchar(m);
+					 for (m = '0' ; m <= '9' ; m++)
+					 {
+						 putchar(m);
+					 }
+				 }
+			 }
+		 }
+
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+void times_table(void)
+{
+	int i = 0;
+	while(i <= 10)
+	{
+		n = '0';
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('\n');
+	} 
+	while(i <= 10)
+        {
+                n = '0' + '1';
+                _putchar(n);
+                _putchar(',');
+                _putchar(' ');
+                _putchar(' ');
+		_putchar('\n');
+        }
+
+}
