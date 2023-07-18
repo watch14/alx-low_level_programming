8bb941e462df67fd4bfe070eff2461d7461c89b7
watch14 <watchman1414@gmail.com>
2023-07-18 14:07:08 +0000

    fast_push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
  *
- * Return: Always 0.
+ * Return: void.
  */
 
 void print_alphabet(void)
