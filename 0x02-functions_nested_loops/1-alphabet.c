6a5310d42ea5f7461bba5e5caa33e79684887151
watch14 <watchman1414@gmail.com>
2023-07-18 13:01:52 +0000

    fast_push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+
+
+int main(void)
+{
+	int a;
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar (a);
+	_putchar ('\n'),
+		return (0);
+}
