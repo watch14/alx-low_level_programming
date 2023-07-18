747f7b49f8ac66510ceb47a49fce53eb21200a57
watch14 <watchman1414@gmail.com>
2023-07-18 12:14:36 +0000

    fast_push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,6 @@
  *
  * Return: 0 indicate success.
  */
-int _putchar()
-{
-	printf("_putchar\n");
-	return(0);
-}
 
 int main(void)
 {
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+int _putchar("_putchar\n");
