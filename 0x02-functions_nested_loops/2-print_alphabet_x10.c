5168eeeba4b74efe3aa94a23ea2d593cb8a06514
watch14 <watchman1414@gmail.com>
2023-07-18 14:20:01 +0000

    fast_push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,16 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet_x10 - Prints the alphabet in lowercase, followed by a new line x10.
  *
  * Return: void.
  */
 
-void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
 	int i, a;
 
-	for(i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		for (a = 'a' ; a <= 'z' ; a++)
 			_putchar (a);
