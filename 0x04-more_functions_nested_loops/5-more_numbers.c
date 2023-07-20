6ff74a05379e6084e26d8dce4a30e01ac99f2d26
watch14 <watchman1414@gmail.com>
2023-07-20 17:47:52 +0000

    fast_push

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,10 @@ void more_numbers(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 0 ; j < 10 ; j++)
-			_putchar (j + '0');
-		for (i = 0 ; i < 5 ; j++)
-		{
-			_putchar ('1');
-			_putchar (j + '0');
+		for (j = 0 ; j <= 14 ; j++){
+			if (j > 9)
+				_putchar ((j / 10) + '0');
+			_putchar ((j % 10) + '0');
 		}
 		_putchar ('\n');
 	}
