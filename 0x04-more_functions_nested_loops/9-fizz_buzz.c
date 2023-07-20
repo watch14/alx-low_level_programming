3ad5eef41783f19b7d6bc06a33e45d3cea5fe350
watch14 <watchman1414@gmail.com>
2023-07-20 23:58:42 +0000

    fast_push

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,15 +17,14 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		if (i % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		if (i % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%d", i);
 		if (i < 100)
 			printf(" ");
 	}
-	printf ("\n");
 	return (0);
 }
