f37e846faeb50b6ea67d7ded92c6c8b08670d71e
watch14 <watchman1414@gmail.com>
2023-07-20 23:29:17 +0000

    fast_push

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square in the terminal.
+ * @size: size of the square
+ */
+
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j < n ; j++)
+			{
+				_putchar ('#');
+			}
+			_putchar ('\n');
+		}
+	}
+}
