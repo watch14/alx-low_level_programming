9b504c4f6f1e96fdb1e2ceae2f671490d4108454
watch14 <watchman1414@gmail.com>
2023-07-20 23:17:46 +0000

    fast_push

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,13 +7,20 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 0 ; i < n ; i++)
-			_putchar (' ');
-		_putchar ('\\');
+		{
+			for (j = 0 ; j < i ; j++)
+			{
+				_putchar (' ');
+			}
+			_putchar ('\\');
+			_putchar ('\n');
+		}
 	}
-	_putchar ('\n');
+	else
+		_putchar ('\n');
 }
