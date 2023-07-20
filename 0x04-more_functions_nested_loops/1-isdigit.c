851a7c5253f7ee4681aac7353c019f37aa9b38f4
watch14 <watchman1414@gmail.com>
2023-07-20 17:06:13 +0000

    fast_push

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <ctype.h>
+
+
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+	else
+		return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,14 @@
 
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
+
+
+
+
+
+
+
+
 
 #endif
