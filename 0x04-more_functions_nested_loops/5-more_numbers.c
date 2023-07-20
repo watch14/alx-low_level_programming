e33c5117963954ed62a12898bf1abbc6694b96da
watch14 <watchman1414@gmail.com>
2023-07-20 17:40:59 +0000

    fast_push

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14; 10 times.
+ *
+ * Return: void.
+ */
+
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+			_putchar (j + '0');
+		for (i = 0 ; i < 5 ; j++)
+		{
+			_putchar ('1');
+			_putchar (j + '0');
+		}
+		-putchar ('\n');
+	}
+}
