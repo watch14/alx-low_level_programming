f9e70fa145d971b0be488b44db150922a942ac6e
watch14 <watchman1414@gmail.com>
2023-07-20 17:11:28 +0000

    fast_push

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * Description: multiply two numbers
+ * @a: interger value
+ * @b: interger value
+ *
+ * Return: result
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
