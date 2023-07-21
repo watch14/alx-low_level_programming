8d9c7d7a34878c85c4f4e573f9623bcc5ebdbf2b
watch14 <watchman1414@gmail.com>
2023-07-21 00:48:02 +0000

    fast_push

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,9 +17,9 @@ void print_triangle(int size)
 
 	else
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 1; j <= size; j++)
+			for (j = 0; j < size; j++)
 			{
 				if (i + j <= size)
 					_putchar(' ');
