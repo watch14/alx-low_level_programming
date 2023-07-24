50924e60a4d69de7ba2ff437b2801cbbaf080c7f
watch14 <watchman1414@gmail.com>
2023-07-24 22:53:13 +0000

    fast_push

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line.
+ * @s: Pointer to the string to be printed in reverse.
+ */
+
+void print_rev(char *s)
+{
+	return(strrev(s));
+}
