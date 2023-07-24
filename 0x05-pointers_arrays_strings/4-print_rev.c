660b8a287c2f7ef538287334a0284156da7c4878
watch14 <watchman1414@gmail.com>
2023-07-24 23:09:34 +0000

    fast_push

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,11 @@
  */
 void print_rev(char *s)
 {
-	int len = strlen(s) , i;
+	int len = strlen(s), i;
 
-	for(i = len - 1 ; i >= 0 ; i--)
+	for (i = len - 1 ; i >= 0 ; i--)
 	{
 		_putchar(s[i]);
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
