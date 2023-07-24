6ebfc5843fcf9161af20804ed32bbe5a41b3d338
watch14 <watchman1414@gmail.com>
2023-07-24 22:43:19 +0000

    fast_push

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+ * _puts - Prints a string to stdout followed by a newline.
+ * @str: Pointer to the string to be printed.
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
