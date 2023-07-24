1cce49f6c9d6e82b991656d2c0732ad59bc34953
watch14 <watchman1414@gmail.com>
2023-07-24 22:31:43 +0000

    fast_push

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+
+
+int _strlen(char *s)
+{
+	int l = strlen(s);
+	return (l);
+}
