fdb07686e36f8b7e7b94138697d12f5e5eb125bf
watch14 <watchman1414@gmail.com>
2023-07-25 00:49:58 +0000

    fast_push

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+void puts_half(char *str)
+{
+	int i, n, len = strlen(str);
+
+	n = (len - 1) / 2
+	if (n % 2 == 1)
+	{
+		for (i = n ; i < len ; i++)
+			_putchar (str[i]);
+	}
+	else
+	{
+		for (i = n + 1 ; i < len ; i++)
+			_putchar (str[i]);
+	}
+}
