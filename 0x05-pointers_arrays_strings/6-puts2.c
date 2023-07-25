157af8ce09fb33c2e595bc10c75a58f95b2e3210
watch14 <watchman1414@gmail.com>
2023-07-25 00:28:07 +0000

    fast_push

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,18 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * puts2 - print every other character of a string,
+ * starting with the first character, followed by a new line.
+ *
+ * @str: The string to be processed.
+ */
+
 void puts2(char *str)
 {
 	int i, len = strlen(str);
 
-	for(i = 0 ; i < len ; i+=2)
-	       _putchar(str[i]);
-	_putchar('\n');
+	for (i = 0 ; i < len ; i+=2)
+	       _putchar (str[i]);
+	_putchar ('\n');
 }
