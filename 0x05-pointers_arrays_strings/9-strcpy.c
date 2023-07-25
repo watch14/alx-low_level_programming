b252d1934fb8f7acaf6b63086eb9e76580d8befa
watch14 <watchman1414@gmail.com>
2023-07-25 01:12:41 +0000

    fast_push

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcpy - Copy a string from src to dest.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string.
+ *
+ * Return: Pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
