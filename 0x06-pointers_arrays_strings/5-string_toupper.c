30ace3f869263c0bf5fbd0f520e612e84550bf36
watch14 <watchman1414@gmail.com>
2023-07-28 01:46:05 +0000

    fast_push

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,13 @@
 
 
 
-char *string_toupper(char *)
-{
-
+char *string_toupper(char *str)
+{	
+	char i;
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		str[i] = touppre(str[i]);
+	}
+	return (str);
+	
 }
