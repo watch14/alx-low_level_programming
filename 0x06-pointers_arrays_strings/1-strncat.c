4e5f6414c0b06c67ffc0d859f31b5c8446212e80
watch14 <watchman1414@gmail.com>
2023-07-28 00:07:41 +0000

    fast_push

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * *_strncat - Concatenates two strings.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ * @n: Maximum number of characters to be concatenated.
+ * Return: Pointer to the resulting string @dest.
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
