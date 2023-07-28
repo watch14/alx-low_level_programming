9307cdd04c03a5ebfca4cedc191f451637bc76e6
watch14 <watchman1414@gmail.com>
2023-07-28 01:04:06 +0000

    fast_push

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-
+/**
+ * reverse_array - revereses an array.
+ * @a: pointer to array.
+ * @n: number of elements of an array.
+ *
+ * Return: void.
+ */
 void reverse_array(int *a, int n)
 {
 	int i, t[n];
