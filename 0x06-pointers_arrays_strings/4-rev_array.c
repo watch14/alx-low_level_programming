8d9fcf3d22628e4c0e510dfe709a58dc8ff044b7
watch14 <watchman1414@gmail.com>
2023-07-28 00:53:36 +0000

    fast_push

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+void reverse_array(int *a, int n)
+{
+	int i, *t;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		t[i] = a[i];
+	}
+
+	for (i = n ; i > 0 ; i++)
+	{
+		a[i] = t[i]
+	}
+}
