455cc053578609064d1746f24f309f16f37d8206
watch14 <watchman1414@gmail.com>
2023-07-28 00:12:51 +0000

    fast_push

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-
+/**
+ * *_strncpy - copies a string
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ * @n: Maximum number of characters to be copied.
+ * Return: Pointer to the resulting string @dest.
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
