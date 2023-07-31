16590f840583f831f3914fbad184cbe78cf3c40c
watch14 <watchman1414@gmail.com>
2023-07-31 10:24:45 +0000

    fast_push

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,15 +5,15 @@
  * @a: Pointer to the 2D array representing the square matrix.
  * @size: The size of the square matrix.
  */
-
 void print_diagsums(int *a, int size)
 {
 	int j, ds1 = 0, ds2 = 0;
 
-	for (j = 0 ; j < size ; j++)
+	for (j = 0; j < size; j++)
 	{
-		ds1 = ds1 + a[j * size +j]
-		ds2 = ds2 + a[j * size + (size - 1 -j)];
+		ds1 += a[j * size + j];
+		ds2 += a[j * size + (size - 1 - j)];
 	}
-	printf ("%d, %d\n", ds1, ds2);
+
+	printf("%d, %d\n", ds1, ds2);
 }
