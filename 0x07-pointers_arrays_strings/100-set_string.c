c5436d40aebe5b0df5c51a21c512ac9c3f807c67
watch14 <watchman1414@gmail.com>
2023-07-31 10:38:21 +0000

    fast_push

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,11 @@
 #include "main.h"
-
+/**
+ * set_string - Sets the value of a pointer to a string.
+ * @s: Pointer to a pointer to a string.
+ * @to: Pointer to the new string.
+ */
 
 void set_string(char **s, char *to)
 {
-	*s = &to;
+	*s = to;
 }
