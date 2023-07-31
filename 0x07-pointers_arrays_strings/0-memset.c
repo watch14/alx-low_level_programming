9bc3a79d77935c5f0b40941884acde701136268d
watch14 <watchman1414@gmail.com>
2023-07-31 08:55:59 +0000

    fast_push

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memeset(s, b, n));
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H      
+#define MAIN_H     
+#include <stdio.h>   
+#include <string.h>   
+#include <ctype.h>  
+
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif
