645f09b45876e1e7b070dcdd48cf6ade0fd46953
watch14 <watchman1414@gmail.com>
2023-08-02 11:05:43 +0000

    fast_push

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string using recursion.
+ * @s: The input string.
+ *
+ * Return: The length of the string (excluding the null terminator).
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + (strlen = _strlen_recursion(s + 1));
+}
