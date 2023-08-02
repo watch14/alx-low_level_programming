b2c08eb3b7e590bf08dad9d58ca5cc3adaeac900
watch14 <watchman1414@gmail.com>
2023-08-02 11:47:50 +0000

    fast_push

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-
+/**
+ * squrareroot - Recursive helper function to find the square root of 'a'.
+ * @a: The number for which to find the square root.
+ * @b: The current value being tested for the square root.
+ *
+ * Return: The square root of 'a' if found, or -1 if 'a' does not have an
+ * integer square root.
+ */
 int squrareroot(int a, int b)
 {
 	if (b * b == a)
