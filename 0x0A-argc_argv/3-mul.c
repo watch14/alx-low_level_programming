7fc90e663a279bda4adabb98780811233f41a8b6
watch14 <watchman1414@gmail.com>
2023-08-04 14:28:54 +0000

    fast_push

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
 * main - print product of argument numbers
 * @argc: argument counter
 * @argv: numbers to multiply
 * Return: 0 on success, 1 if two arguments not given
-*/
-
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
