654c5caa33f815b401def60470915af6fd5493d7
watch14 <watchman1414@gmail.com>
2023-08-04 14:20:48 +0000

    fast_push

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments (including the program name)
+ * @argv: An array containing the command-line arguments as strings
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc ,char *argv[])
+{
+	(void)argc;
+	int i;
+
+	for (i = 0 ; i < argv ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	retrun (0);
+}
