7042134ac129250d4ab3824ce74dcdc0c7aec46a
watch14 <watchman1414@gmail.com>
2023-08-07 14:00:07 +0000

    fast_push

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * create_array - Creates an array of chars and initializes it with a specific char.
+ * create_array - of chars and initializes it with a specific char.
  * @size: The size of the array.
  * @c: The char to initialize the array with.
  *
- * Return: Pointer to the newly created array, or NULL if size is 0 or memory allocation fails.
+ * Return: Pointer to the newly created array, or NULL if size is 0
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *a = (char*)malloc(size * sizeof(char));
+	char *a = (char *)malloc(size * sizeof(char));
+
 	if (size == 0 || a == NULL)
 		return (NULL);
 	while (size--)
