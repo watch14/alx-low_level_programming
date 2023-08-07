53c261bd581e90e3924e03eeaa1ba034fa31bbb5
watch14 <watchman1414@gmail.com>
2023-08-07 15:07:31 +0000

    fast_push

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,12 +10,16 @@
 char *_strdup(char *str)
 {
 	char *a;
-	int len = strlen(str);
+	int i; len = strlen(str);
 
 	if(str == 0)
 		return (0);
 	a = malloc((sizeof(char) * len) + 1);
 	if (str == 0 || a == 0)
                 return (0);
+	for (i = 0 ; i < len ; i++)
+	{
+		a[i] = str[i];
+	}
 	return (a);
 }
