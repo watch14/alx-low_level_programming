79e9b0f4f09c049f0df92ae964503c2c5e81e809
watch14 <watchman1414@gmail.com>
2023-08-10 16:27:06 +0000

    fast_push

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,30 @@
-#include "main.h"
-
-/**
- * string_nconcat - Concatenates two strings up to n bytes from s2
- * @s1: The first string
- * @s2: The second string
- * @n: The maximum number of bytes to concatenate from s2
- *
- * Return: A pointer to the concatenated string, or NULL on failure
- */
+#include <stdlib.h>
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *t;
-	int l1, l2, j, i;
+	unsigned int l1, l2, j, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	/*to calculate the lenghth of the strings*/
+
 	for (l1 = 0 ; s1[l1] != '\0' ; l1++)
 		;
 	for (l2 = 0 ; s2[l2] != '\0' ; l2++)
 		;
 	if (l2 < n)
-		return (NULL);
-	/*to reserve the memory*/
-	t = malloc((l1 + n + 1) * sizeof(char));
-	if (!t)
-		return (NULL);
-	/*to put s2 and s2 into t*/
+		return NULL;
+
+	t = malloc(l1 + n + 1);
+	if (t == NULL)
+		return NULL;
+
 	for (i = 0 ; i < l1 ; i++)
 		t[i] = s1[i];
-	for (j = 0 ; j < n ; j++, i++)
-		t[i] = s2[j];
-	t[i] = 0;
-	return (t);
+	for (j = 0 ; j < n ; j++)
+		t[i + j] = s2[j];
+	t[i + j] = '\0';
+	return t;
 }
