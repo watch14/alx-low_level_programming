603ad868e07859feacf405f106c2d94e0e27e74c
watch14 <watchman1414@gmail.com>
2023-08-10 16:37:07 +0000

    fast_push

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+        char *p = m;
+
+        while (*m)
+                m++;
+        return (m - p);
+}
+/**
  * string_nconcat - concatenates two strings
  * @s1: destination string
  * @s2: source string
@@ -8,29 +21,25 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i, j, len, l1, l2;
 	char *t;
-	unsigned int l1, l2, j, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	for (l1 = 0 ; s1[l1] != '\0' ; l1++)
-		;
-	for (l2 = 0 ; s2[l2] != '\0' ; l2++)
-		;
-	if (l2 < n)
-		return (NULL);
-
-	t = malloc(l1 + n + 1);
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	if (n > l2)
+		n = l2;
+	len = l1 + n;
+	t = malloc(sizeof(char) * len + 1);
 	if (!t)
 		return (NULL);
-
-	for (i = 0 ; i < l1 ; i++)
+	for (i = 0; i < l1; i++)
 		t[i] = s1[i];
-	for (j = 0 ; j < n && s2[j] != '\0'; j++)
-		t[i + j] = s2[j];
-	t[i + j] = '\0';
+	for (j = 0; j < n; j++, i++)
+		t[i] = s2[j];
+	t[i] = 0;
 	return (t);
 }
