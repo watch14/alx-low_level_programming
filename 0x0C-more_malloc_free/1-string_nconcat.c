609eb331a56fc7d4d100aa5d35224c849d35bcf0
watch14 <watchman1414@gmail.com>
2023-08-10 15:48:18 +0000

    fast_push

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,29 +13,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *t;
 	int l1, l2;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-                s2 = "";
-	
-	//to calculate the lenghth of the strings
+		s2 = "";
+	/*to calculate the lenghth of the strings*/
 	for (l1 = 0 ; s1[l1] != \0 ; l1++)
 		;
 	for (l2 = 0 ; s2[l2] != \0 ; l2++)
-                ;
+		;
 	if (l2 < n)
-                return (0);
-
-	//to reserve the memory
+		return (0);
+	/*to reserve the memory*/
 	t = malloc(l1 + n + 1);
 	if (t == NULL)
 		return (0);
-	
-	//to put s1 and s2 into t
+	/*to put s1 and s2 into t*/
 	strcpy(t, s1);
 	strcat(t, s2);
-
-	
 	return (t);
 }
