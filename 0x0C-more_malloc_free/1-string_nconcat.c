f319e8af3baf2b9bf2f828f4ab8111bb7fec5aaf
watch14 <watchman1414@gmail.com>
2023-08-10 16:28:26 +0000

    fast_push

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
-
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: destination string
+ * @s2: source string
+ * @n: number of bytes from s2 to be copied
+ * Return: concatenated string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *t;
@@ -15,16 +21,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (l2 = 0 ; s2[l2] != '\0' ; l2++)
 		;
 	if (l2 < n)
-		return NULL;
+		return (NULL);
 
 	t = malloc(l1 + n + 1);
 	if (t == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0 ; i < l1 ; i++)
 		t[i] = s1[i];
 	for (j = 0 ; j < n ; j++)
 		t[i + j] = s2[j];
 	t[i + j] = '\0';
-	return t;
+	return (t);
 }
