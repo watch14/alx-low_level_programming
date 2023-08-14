e80a659e064ddd94a1b70bf413b3c2c87d822b63
watch14 <watchman1414@gmail.com>
2023-08-14 09:20:30 +0000

    fast_push

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,13 +10,13 @@
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d)
 	{
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		printf("Name: %s\n", d->name);
 		if (d->age == NULL)
-			printf("Age: (nil)\n");
+			printf("Age: 0\n");
 		printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
