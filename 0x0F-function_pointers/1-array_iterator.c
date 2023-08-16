6941dd8bff6bf58449992cc2e23619204b30d0d2
watch14 <watchman1414@gmail.com>
2023-08-16 09:25:53 +0000

    fast_push

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,14 +8,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
 	if (array && size && cmp)
-	for (i = 0 ; i < size ; i++)
-	{
-		if (cmp(array[i]))
-			return (i);
-	}
-
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
 	return (-1);
 }
