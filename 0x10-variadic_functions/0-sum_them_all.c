b0e7c19d1e1539acfe670b6b989177bdd39866ac
watch14 <watchman1414@gmail.com>
2023-08-17 10:02:55 +0000

    fast_push

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,18 +11,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    va_start(args, n);
-    
-    int sum = 0;
-    unsigned int i;
-    
-    for (i = 0; i < n; i++)
-    {
-        int x = va_arg(args, int);
-        sum += x;
-    }
-    
-    va_end(args);
-    return (sum);
+	va_list args;
+	va_start(args, n);
+
+	int sum = 0;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		int x = va_arg(args, int);
+		sum += x;
+	}
+
+	va_end(args);
+	return (sum);
 }
