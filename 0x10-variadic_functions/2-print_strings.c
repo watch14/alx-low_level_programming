eead29115e2a0c7a2d45181ea56898ea9f87a65e
watch14 <watchman1414@gmail.com>
2023-08-17 11:27:30 +0000

    fast_push

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,27 +6,29 @@
  * @n: The number of strings to print
  * @...: The variable arguments containing the strings
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
 	unsigned int i;
+
+	va_list args;
 
 	va_start(args, n);
 
-	for (i = 0 ; i < n ; i++)
+	for (i = 0; i < n; ++i)
 	{
 		char *s = va_arg(args, char *);
 
 		if (s == NULL)
 			printf("(nil)");
+		else
+			printf("%s", s);
 
-		printf("%s", s);
-
-		if (i != (n - 1) && separator != NULL)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
-
 	}
-	printf("\n");
+
 	va_end(args);
+
+	printf("\n");
 }
+
