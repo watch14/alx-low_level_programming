fe203a7d6c6ee8a0b4fe0e4d7d5da56e5983295c
watch14 <watchman1414@gmail.com>
2023-08-17 10:01:22 +0000

    fast_push

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of all its parameters.
+ * @n: The number of arguments passed to the function.
+ * @...: The variable arguments.
+ *
+ * Return: The sum of the variable arguments.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list args;
+    va_start(args, n);
+    
+    int sum = 0;
+    unsigned int i;
+    
+    for (i = 0; i < n; i++)
+    {
+        int x = va_arg(args, int);
+        sum += x;
+    }
+    
+    va_end(args);
+    return (sum);
+}
