e60d6df29b8a1c6f9db635cf202e3d8e646107c4
watch14 <watchman1414@gmail.com>
2023-08-17 10:45:52 +0000

    fast_push

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,26 +8,19 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-	int i = 0;
+    va_list args;
+    unsigned int i = 0;
 
-	va_start(args, n);
+    va_start(args, n);
 
-	if (!n)
-	{
-		printf("\n");
-		return;
-	}
+    for (; i < n ; i++)
+    {
+        int x = va_arg(args, int);
+        printf("%d", x);
 
-	for (; i < n ; i++)
-	{
-		int x = va_arg(args, int);
-
-		printf("%d", x);
-		if (i < n - 1)
-			printf("%s", separator)
-
-	}
-	va_end(args);
-	printf("\n");
+        if (i < n - 1)
+            printf("%s", separator);
+    }
+    va_end(args);
+    printf("\n");
 }
