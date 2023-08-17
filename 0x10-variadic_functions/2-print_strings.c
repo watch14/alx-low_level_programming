f86094149095556147ea8e71df2294b8c302458f
watch14 <watchman1414@gmail.com>
2023-08-17 11:25:07 +0000

    fast_push

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,13 +11,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	char *s;
 
 	va_start(args, n);
 
 	for (i = 0 ; i < n ; i++)
 	{
-		s = va_arg(args, char *);
+		char *s = va_arg(args, char *);
 
 		if (s == NULL)
 			printf("(nil)");
