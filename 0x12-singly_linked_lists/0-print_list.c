7d8959dc7d330486a3ead9d6ca9aa27a2cc7fd42
watch14 <watchman1414@gmail.com>
2023-08-24 13:52:28 +0000

    fast_push

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include <lists.h>
+#include "lists.h"
 
 /**
  * print_list - Prints all the elements of a list_t list.
@@ -8,7 +8,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
@@ -22,7 +22,8 @@ size_t print_list(const list_t *h)
 		}
 
 		h = h->next;
-		i++;
+		count++;
 	}
-	return (i);
+
+	return (count);
 }
