c9d3bcfa4a237ae8f9049489a94cb66d1a9a45a2
watch14 <watchman1414@gmail.com>
2023-08-24 14:39:23 +0000

    fast_push

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,29 @@
 #include "lists.h"
-
 /**
- * str_len - function that returns the length of a string.
- * @s : s is a character
- * Return: value is i
+ * add_node - add a new node at the beginning of a list_t list.
+ * @head: pointer to a pointer to the head of a list_t list.
+ * @str: value to insert into the new element.
+ * Return: pointer to the new node.
  */
-
-int str_len(const char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * add_node - add a new node at beginning of a list_t list.
- * @head: head of a list_t list.
- * @str: value to insert into element.
- * Return: the number of nodes.
- */
-
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t new;
+	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	if (add == NULL)
-		return (NULL);
+	if (new == NULL)
+		return NULL;
+
 	new->str = strdup(str);
-	new->len = str_len(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return NULL;
+	}
+
+	new->len = strlen(str);
 	new->next = *head;
 	*head = new;
-	return (add);
+
+	return new;
 }
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,7 +25,6 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-int str_len(char *str)
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
