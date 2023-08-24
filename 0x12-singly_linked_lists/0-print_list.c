ca8179f10c4dc666050732ce38b740e2a50ccf16
watch14 <watchman1414@gmail.com>
2023-08-24 13:41:55 +0000

    fast_push

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,13 @@ size_t print_list(const list_t *h)
 
 	while (h->next != NULL)
 	{
-		printf("[%d] %s\n",h->len, h->str);
-
 		if (h->str == NULL)
 			printf("[0](nil)\n");
+		
+		printf("[%d] %s\n",h->len, h->str);
 
 		h = h->next;
-		i++:
+		i++;
 	}
-	retutn (i);
+	return (i);
 }
