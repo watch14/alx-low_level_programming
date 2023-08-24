daa1d1186074167c3c6292957e05dda973ddaab7
watch14 <watchman1414@gmail.com>
2023-08-24 14:02:53 +0000

    fast_push

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - Counts the number of elements in a linked list.
+ * @h: A pointer to the head of the list.
+ *
+ * Return: The number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+        int i = 0;
+
+        while (h != NULL)
+        {
+                i++;
+                h = h->next;
+        }
+        return (i);
+}
