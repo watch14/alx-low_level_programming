72bdc6b25a08b0c9dbe39577a71e7e391ea5dd9b
watch14 <watchman1414@gmail.com>
2023-08-24 14:03:36 +0000

    fast_push

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
 
 size_t list_len(const list_t *h)
 {
-        int i = 0;
+	int i = 0;
 
-        while (h != NULL)
-        {
-                i++;
-                h = h->next;
-        }
-        return (i);
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
