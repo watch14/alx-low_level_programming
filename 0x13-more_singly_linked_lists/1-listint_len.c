065bc472ad6fb3542a680099062c172435dc3e06
watch14 <watchman1414@gmail.com>
2023-08-28 22:40:11 +0000

    fast_push

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - Counts the number of elements in a linked list.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: The number of elements in the linked list.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+
+	int i = 0;
+	const listint_t *curent = h;
+
+	while (curent != NULL)
+	{
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
