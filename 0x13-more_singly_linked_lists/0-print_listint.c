29b7f8ed3379395996c38871ccf212961cd3abd9
watch14 <watchman1414@gmail.com>
2023-08-28 22:29:48 +0000

    fast_push

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints the elements of a linked list and counts them.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: The number of elements in the linked list.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+	const listint_t *current = h;
+
+	while (current->next != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
+
