3c48b54f011b65918f929baf695aa14f11617a1c
watch14 <watchman1414@gmail.com>
2023-08-29 18:58:35 +0000

    fast_push

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Calculates the sum of all integers in a listint_t linked list.
+ * @head: Pointer to the head of the linked list.
+ *
+ * Return: Sum of all integers in the linked list, or 0 if the list is empty.
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *node;
+	int sum = 0;
+
+	node = head;
+
+	if (!node)
+		return (NULL);
+
+	while (node)
+	{
+		sum = node->n;
+		node = node->next;
+	}
+	return (sum);
+}
