0ff9645d42e163897c02500a2889f6ddfe810c05
watch14 <watchman1414@gmail.com>
2023-08-29 17:28:40 +0000

    fast_push

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,19 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
+	listint_t *current, *temp;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return;
 
-	while (*head != NULL)
+	current = *head;
+
+	while (current != NULL)
 	{
-		current = *head;
-		*head = head->next;
-		free(current);
+		temp = current;
+		current = current->next;
+		free(temp);
 	}
+
 	*head = NULL;
 }
