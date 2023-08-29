645c801789af9e90a266cc6b499190fd22164066
watch14 <watchman1414@gmail.com>
2023-08-29 19:29:43 +0000

    fast_push

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (tmp)
 	{
-		if (i == idx - 1)
+		if (i == idx)
 		{
 			node = malloc(sizeof(listint_t));
 			if (!node)
