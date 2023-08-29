fe78fc96f52a7e1834d5c866e43dbd1fded84360
watch14 <watchman1414@gmail.com>
2023-08-29 19:24:03 +0000

    fast_push

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!tmp)
 		return (NULL);
 
+	if (idx == 0 || *head == NULL)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+
 	while (tmp)
 	{
 		if (i == idx)
