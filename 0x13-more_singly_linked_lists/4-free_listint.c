1177edf1803efdb26885c5d7b0c22c117624f709
watch14 <watchman1414@gmail.com>
2023-08-29 17:22:24 +0000

    fast_push

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a linked list.
+ * @head: Pointer to the head of the linked list.
+ *
+ * Description: Frees all nodes in a linked list and
+ * deallocates the memory used by each node.
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (current != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
