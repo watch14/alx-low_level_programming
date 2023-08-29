b164016fe835633d1d4fc509a8f07b0dba42a9d8
watch14 <watchman1414@gmail.com>
2023-08-29 19:56:51 +0000

    fast_push

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,7 +7,6 @@
  *
  * Return: 1 if successful, -1 if failed.
  */
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *node, *tmp;
@@ -19,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		node = *head;
-		*head = head->next;
+		*head = (*head)->next;
 		free(node);
 		return (1);
 	}
@@ -40,3 +39,4 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	return (1);
 }
+
