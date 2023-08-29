6fd229cac37aa40ed0fe8f0473cacea2542bf2dc
watch14 <watchman1414@gmail.com>
2023-08-29 17:09:34 +0000

    fast_push

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,8 +10,9 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *node = malloc(sizeof(listint_t));
+	listint_t *node;
 
+	node = malloc(sizeof(listint_t));
 	if (node == NULL)
 		return (NULL);
 
