21478736878e7a9afdeb7ecf8a47e1124f9e839b
watch14 <watchman1414@gmail.com>
2023-08-29 17:00:12 +0000

    fast_push

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 
-
+/**
+ * add_nodeint_end - Adds a new node at the end of a linked list.
+ * @head: Pointer to the pointer of the head of the linked list.
+ * @n: Value to be stored in the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
@@ -11,6 +17,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	node->n = n;
 	node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
 
 	listint_t *current = *head;
 
