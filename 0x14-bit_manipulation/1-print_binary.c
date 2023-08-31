f7469e60b8e423b0a49fbbe6caa79373d473f5a2
watch14 <watchman1414@gmail.com>
2023-08-31 21:32:32 +0000

    fast_push

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,3 +1,24 @@
 #include "main.h"
 
+/**
+ * print_binary - Prints the binary representation of an unsigned long integer.
+ * @n: The unsigned long integer to be printed in binary.
+ */
 
+void print_binary(unsigned long int n)
+{
+	int b = sizeof(n) * 8, r = 0;
+
+	while (b)
+	{
+		if (n & 1L << --b)
+		{
+			putchar ('1');
+			r++;
+		}
+		if (r)
+			putchar ('0');
+	}
+	if (!n)
+		putchar ('0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,8 +3,6 @@
 
 #include <stdio.h>
 
-
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -13,5 +11,17 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 #endif
