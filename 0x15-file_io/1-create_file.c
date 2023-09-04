658102649e4c5d12deab86b8f842cda4c7699f9f
watch14 <watchman1414@gmail.com>
2023-09-04 09:04:02 +0000

    fast_push

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	len = 0;
-	while (text_content[len] != '0')
+	while (text_content[len] != '\0')
 	{
 		len++;
 	}
