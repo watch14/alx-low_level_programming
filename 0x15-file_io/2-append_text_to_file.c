e148df6c6d9cdb59cec6d5fbd3a7d96887572086
watch14 <watchman1414@gmail.com>
2023-09-04 09:39:42 +0000

    fast_push

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int f, len = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
