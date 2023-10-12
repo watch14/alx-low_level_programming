35d2145e6ecada26018288a4de1837280c9e9398
watch14 <watchman1414@gmail.com>
2023-10-12 17:46:40 +0000

    fast_push

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all elements of a dlistint_t list
+ * @h: Head of the linked list
+ * Return: The number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int size = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		size++;
+	}
+	return (size);
+}
