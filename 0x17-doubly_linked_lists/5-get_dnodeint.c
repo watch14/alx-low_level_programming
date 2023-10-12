d33b4dc315fc523e1a0b5d9c0ada363587dfbe1d
watch14 <watchman1414@gmail.com>
2023-10-12 18:00:55 +0000

    fast_push

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,15 +9,15 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    while (head != NULL)
-    {
-        if (i == index)
-            return head;
-        head = head->next;
-        i++;
-    }
+	while (head != NULL)
+	{
+		if (i == index)
+			return head;
+		head = head->next;
+		i++;
+	}
 
-    return NULL;
+	return NULL;
 }
