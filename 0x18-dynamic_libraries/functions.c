5953c01943e66ab8aa89d5c7ffee9a34b3c34452
watch14 <watchman1414@gmail.com>
2023-10-16 05:29:36 +0000

    fast_push

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
+int div(int x, int y)
+{
+	if (y == 0)
+	{
+		printf("Error : cant devide by 0\n");
+		return (0);
+	}
+	return (x / y);
+}
+
+int mod(int x, int y)
+{
+	if (y == 0)
+	{
+		printf("Error : cant devide by 0\n");
+		return (0);
+	}
+	return (x % y);
+}
