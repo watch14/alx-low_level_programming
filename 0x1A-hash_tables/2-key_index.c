73f3f9f05a146fc78c8eb49e5da1e48992d6a64c
watch14 <watchman1414@gmail.com>
2023-11-02 21:58:40 +0000

    fast_push

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Get the index of a key in the hash table
+ * @key: The key to hash
+ * @size: Size of the hash table
+ *
+ * Return: Index of the key in the hash table.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size)
+}
