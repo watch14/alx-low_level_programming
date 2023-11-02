89cf408aaf1d78a4df69333cde286f63b9b48940
watch14 <watchman1414@gmail.com>
2023-11-02 22:52:11 +0000

    fast_push

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (key == NULL || strlen(key) == 0 || value == NULL ||
 			ht == NULL || ht->array == NULL || ht->size == 0)
 		return (0);
-	index = hash_djb2((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 
 	while (temp != NULL)
