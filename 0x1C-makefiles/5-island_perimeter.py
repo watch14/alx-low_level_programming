e929cee02e4b371dd3ff55f22d0433d86c5eb937
watch14 <watchman1414@gmail.com>
2023-12-20 06:35:40 +0000

    fast_push

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Island perimeter """
+
+def island_perimeter(grid):
+    """ Calculates the perimeter of the island """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
